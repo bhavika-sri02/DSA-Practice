aa00249bbd2e732acd8b777a58296ab92a9f4a31
Bhavika <bhavikasrivastava2@gmail.com>
2026-09-22 14:18:26 +0530

    Binary search modified

--- javaPractice/src/javaPractice/BinarySearch.java
+++ javaPractice/src/javaPractice/BinarySearch.java
@@ -3,8 +3,29 @@ package javaPractice;
 public class BinarySearch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int[] a= {2,4,6,8,10,11,12,13,15};
+		int t = 6;
+		int index= binarySearch(a,t);
+		if (index!= -1)
+			System.out.print("Element found at index: "+index);
+		else
+			System.out.print("Element NOT found!!");
 	}
-
+	static int binarySearch(int[] a,int t )
+	{
+		int start=0,end=a.length-1;
+		int mid;
+		for (int i=0;i<a.length;i++)
+		{
+			mid=(start+end)/2;
+			if(a[mid] == t)
+				return mid;
+			else if(a[mid]>t)
+				end=mid-1;
+			else
+				start=mid+1;
+		}
+		return -1;
+	}
 }
