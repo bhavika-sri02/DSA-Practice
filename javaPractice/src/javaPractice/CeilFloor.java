bd19b69ed32319ef6c3e60a696a61cc319a4c822
Bhavika <bhavikasrivastava2@gmail.com>
2026-09-24 22:26:38 +0530

    first and last occurrence in array - finite and infinite

--- javaPractice/src/javaPractice/CeilFloor.java
+++ javaPractice/src/javaPractice/CeilFloor.java
@@ -4,18 +4,21 @@ public class CeilFloor {
 
 	public static void main(String[] args) {
 		int a[]= {1,3,22,39,55,107,111,133};
-		int t=108;
-		int ceil=ceiling(a,t);
-		System.out.println("Ceiling value: "+ceil);
-
+		int t=1080;
+		System.out.println("Ceil value: " + ceiling(a,t));
+		System.out.println("Floor value: " + floor(a,t));
 	}
-	static int ceiling(int a[], int t)
+	
+	//returns largest element smalller than the number
+	static int floor(int[] a, int t) 
 	{
-		//returns smallest element larger than the number
-		int c=0;
+		//floor value does not exist if the target is smaller than smallest element
+		if(t < a[0]) 
+			return -1;
+		
 		int start=0,end=a.length-1;
 		int mid;
-		for(int i=0;i<a.length;i++)
+		while(start<=end)
 		{
 			mid=start+(end-start)/2;
 			if(a[mid]==t)
@@ -23,15 +26,66 @@ public class CeilFloor {
 			else if(a[mid]<t)
 			{
 				start=mid+1;
-				c=a[mid];
 			}
 			else 
 			{
 				end=mid-1;
-				c=a[mid];
 			}
 		}
-		return c;
+		return a[end];
 	}
+	
+	
+	//returns smallest element larger than the number
+	static int ceiling(int a[], int t)
+	{
+		//ceil value does not exist if the target is larger than largest element
+		if(t > a[a.length-1]) 
+			return -1;
+		
+		int start=0,end=a.length-1;
+		int mid;
+		while(start<=end)
+		{
+			mid=start+(end-start)/2;
+			if(a[mid]==t)
+				return a[mid];
+			else if(a[mid]<t)
+			{
+				start=mid+1;
+			}
+			else 
+			{
+				end=mid-1;
+			}
+		}
+		return a[start];
+	}
+	
+	//BRUTE FORCE APPROACH
+//	static int ceiling(int a[], int t)
+//	{
+//		//returns smallest element larger than the number
+//		int c=0;
+//		int start=0,end=a.length-1;
+//		int mid;
+//		for(int i=0;i<a.length;i++)
+//		{
+//			mid=start+(end-start)/2;
+//			if(a[mid]==t)
+//				return a[mid];
+//			else if(a[mid]<t)
+//			{
+//				start=mid+1;
+//				c=a[mid];
+//			}
+//			else 
+//			{
+//				end=mid-1;
+//				c=a[mid];
+//			}
+//		}
+//		return c;
+//	}
 
 }
---
+++ javaPractice/src/javaPractice/InfiniteArray.java
@@ -0,0 +1,51 @@
+package javaPractice;
+
+import java.util.Arrays;
+
+public class InfiniteArray {
+
+	public static void main(String[] args) {
+		//TO FIND THE OCCURRENCE OF AN ELEMENT IN AND INFINITE ARRAY
+		//infinite array means we cannot know the last index any how
+		//so work on partition-based approach by doubling each time the element not found
+		int a[]= {1,2,11,12,21,33,44,55,66,77,88,999};
+		int target= 11;
+		System.out.println("ARRAY:\t"+Arrays.toString(a));
+		System.out.println("Target at index:\t"+ans(a,target));
+	}
+	static int ans(int a[],int target)
+	{
+		int start=0, end=1, index=-1, temp;
+		while(a[end]<target)
+		{
+			temp=end+1;
+			end=end+(end-start+1)*2;
+			start=temp;
+			index=findIndex(a,target,start,end);
+		}
+		return index;
+	}
+	static int findIndex(int[] a, int t, int start, int end) {
+		
+		int mid;
+		int index=-1;
+		while(start<=end)
+		{
+			mid=start+(end-start)/2;
+			if(a[mid]==t)
+			{
+				return mid;
+			}
+			else if(a[mid]<t)
+			{
+				start=mid+1;
+			}
+			else 
+			{
+				end=mid-1;
+			}
+		}
+		return index;
+	}
+
+}
